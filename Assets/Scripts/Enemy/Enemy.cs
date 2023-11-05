872c4c65e168d1d2bbf6dad89cfb9be87c2f603a
Ivanisimus <114506587+Ivanisimus@users.noreply.github.com>
2023-11-05 23:49:01 +0300

    Enemy buffs

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -7,6 +7,7 @@ using UnityEngine.UIElements;
 public class Enemy : MonoBehaviour
 {
     public float Speed;
+    public float speedM = 1;
     private Transform player;
 
     public bool isPlayer;
@@ -21,12 +22,40 @@ public class Enemy : MonoBehaviour
 
     public int hp;
 
+    public GameObject d1, d2, d3, d4, d5, d6;
+    GameObject drop;
+    public int type;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         anim = GetComponent<Animator>();
+
+        int randomNumber2 = Random.Range(1, 101);
+        if (randomNumber2 >= 1 && randomNumber2 < 31)
+        {
+            type = 1;
+        }
+        if (randomNumber2 >= 31 && randomNumber2 < 56)
+        {
+            type = 2;
+        }
+        if (randomNumber2 >= 56 && randomNumber2 < 76)
+        {
+            type = 3;
+        }
+        if (randomNumber2 >= 76 && randomNumber2 < 91)
+        {
+            type = 4;
+        }
+        if (randomNumber2 >= 91 && randomNumber2 < 101)
+        {
+            type = 5;
+        }
+
+        gameObject.GetComponent<EnemyBuffs>().type = type;
     }
 
     // Update is called once per frame
@@ -44,7 +73,7 @@ public class Enemy : MonoBehaviour
     {
         if (isPlayer)
         {
-            transform.position = Vector2.MoveTowards(transform.position, player.position, Speed * Time.deltaTime);
+            transform.position = Vector2.MoveTowards(transform.position, player.position, Speed * speedM * Time.deltaTime);
             
         }
 
@@ -63,7 +92,45 @@ public class Enemy : MonoBehaviour
         hp--;
         if (hp <= 0)
         {
+            EnemyDrop();
             Destroy(gameObject);
+        }
+    }
+
+    public void EnemyDrop()
+    {
+        int randomNumber = Random.Range(1, 4);
+        if(randomNumber == 1)
+        {
+            int randomNumber2 = Random.Range(1, 101);
+            if (randomNumber2 >= 1 && randomNumber2 < 31)
+            {
+                Instantiate(d1, transform.position, Quaternion.Euler(0, 0, 0));
+            }
+            if (randomNumber2 >= 31 && randomNumber2 < 56)
+            {
+                Instantiate(d2, transform.position, Quaternion.Euler(0, 0, 0));
+            }
+            if (randomNumber2 >= 56 && randomNumber2 < 76)
+            {
+                Instantiate(d3, transform.position, Quaternion.Euler(0, 0, 0));
+            }
+            if (randomNumber2 >= 76 && randomNumber2 < 91)
+            {
+                Instantiate(d4, transform.position, Quaternion.Euler(0, 0, 0));
+            }
+            if (randomNumber2 >= 91 && randomNumber2 < 101)
+            {
+                Instantiate(d5, transform.position, Quaternion.Euler(0, 0, 0));
+            }
+        }
+        else
+        {
+            int randomNumber3 = Random.Range(1, 11);
+            if(randomNumber3 == 1)
+            {
+                Instantiate(d6, transform.position, Quaternion.Euler(0, 0, 0));
+            }
         }
     }
 }
