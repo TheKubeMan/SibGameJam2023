f080c8025da4bf2a3a87ff28cf1bddb2315011c8
Ivanisimus <114506587+Ivanisimus@users.noreply.github.com>
2023-11-06 16:32:39 +0300

    Full enemies and boss

---
+++ Assets/Scripts/Enemy/Boss.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    public float Speed;
+    public float speedM = 1;
+    private Transform player;
+
+    public LayerMask whatIsPlayer;
+
+    private Rigidbody2D rb;
+
+    private Animator anim;
+
+    public int health;
+
+    public GameObject deathEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        anim = GetComponent<Animator>();
+        anim.SetInteger("boss", 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player.position, Speed * speedM * Time.deltaTime);
+
+        if (player.transform.position.x > transform.position.x)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        else
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+    }
+
+    public void DamageBoss()
+    {
+        health--;
+        if (health <= 0)
+        {
+            Instantiate(deathEffect, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+
+    public void ChangeHP(int newHP)
+    {
+        health = newHP;
+    } 
+}
