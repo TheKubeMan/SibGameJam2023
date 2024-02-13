9940d7e5fc5e5f29beac6b5f706c1398fabd7e9e
Ivanisimus <114506587+Ivanisimus@users.noreply.github.com>
2024-02-13 21:46:59 +0300

    New_Feaches

---
+++ Assets/Scripts/GamePause.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePause : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+    public GameObject pause;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pause.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    public void Pause()
+    {
+        pause.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+}
